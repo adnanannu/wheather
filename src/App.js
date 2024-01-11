e42aa004999f07dbd5e20eaef1e70ec70fa928b9
shammasashra <shammasashraf01@gmail.com>
2024-01-11 12:37:39 +0530

    wheather

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import WheaterApp from './WheaterApp';
+
+function App() {
+  return (
+    <div>
+      <WheaterApp/>
+    </div> 
+    
+  );
+}
+
+export default App;
